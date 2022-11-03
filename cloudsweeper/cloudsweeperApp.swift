8056aa08a009537f07835bab7eea134b2e82ff7f
Krithik Rao <krdevmail@gmail.com>
2022-11-02 20:11:28 -0700

    Initial Commit

---
+++ cloudsweeper/cloudsweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudsweeperApp.swift
+//  cloudsweeper
+//
+//  Created by Krithik Rao on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct cloudsweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
